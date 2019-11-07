c4d4a744e3dcecaf7901e2d9d73ee30878434312
kibehiro <k565565@kansai-u.ac.jp>
2019-11-07 20:56:30 +0900

    committed in 2019/11/07 20:56:30

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,12 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
+    const userName = $('#userName').val();
     // ユーザ名が未入力でないかチェックする
-
-
-    $('form').submit();
+    console.log(userName);
+    if (userName !== ''){
+      $('form').submit();
+    }else{
+      alert('ユーザ名を入力してください');
+    }
 }
