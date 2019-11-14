cac6849db9d4b4090c74f368f5bb5f81c1009973
kei-uma <umakoshi.keisuke.uj7@is.naist.jp>
2019-11-14 18:22:10 +0900

    committed in 2019/11/14 18:22:10

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,11 +3,11 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message').val();
     // メモの内容を表示
-    $('#thread').prepend('<p>' + '</p>');
-
+    $('#thread').prepend('<p>'+userName+'さんのメモ：'+ message+ '</p>');
+    document.getElementById("message").value = '';
     return false;
 }
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,13 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
+    socket.on('sendExitMessageEvent', function (data) {
+      if (!data) {
+          return;
+      }
 
+      console.log('クライアントの入力値：' + data);
+      // 全クライアントが受信するメッセージ表示イベント（receiveMessageEvent）を送信する
+      socket.broadcast.emit('receiveMessageEvent',data);
     });
 };
