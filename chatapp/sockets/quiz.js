b19f34efcbf53a77f5ac4effadf0f5da7ee4d0f9
kibehiro <k565565@kansai-u.ac.jp>
2019-11-21 19:26:35 +0900

    クイズ機能の実装

--- chatapp/sockets/quiz.js
+++ chatapp/sockets/quiz.js
@@ -1,12 +1,33 @@
 'use strict';
 
+const quiz_db = [
+  {
+    "question": "りんごのいろは？",
+    "answer": "赤色"
+  },
+  {
+    "question": "信号機の進めの色は？",
+    "answer": "青色"
+  },
+  {
+    "question": "株式会社ラクスが上場する株式市場は？",
+    "answer": "東証マザーズ"
+  }
+]
+
 module.exports = function (socket, io) {
-    // 退室メッセージをクライアントに送信する
-    socket.on('sendQuizMessageEvent', function (data) {
-      if (!data) {
-          return;
-      }
-      // 全クライアントが受信するメッセージ表示イベント（receiveQuizMessageEvent）を送信する
-      io.sockets.emit('receiveQuizMessageEvent',data);
-    });
+  // 退室メッセージをクライアントに送信する
+  socket.on('sendQuizMessageEvent', function (data) {
+    if (!data) {
+      return;
+    }
+    let quiz = quiz_db[Math.floor(Math.random() * quiz_db.length)];
+    data = {
+      data,
+      quiz
+    };
+    // 全クライアントが受信するメッセージ表示イベント（receiveQuizMessageEvent）を送信する
+    io.sockets.emit('receiveQuizMessageEvent', data);
+    io.sockets.emit('receiveQuizAnswer', data.quiz['answer']);
+  });
 };
