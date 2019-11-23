16f2b85dea5709e35c3cb2186031460f2b8eeb56
kibehiro <k565565@kansai-u.ac.jp>
2019-11-24 00:23:29 +0900

    名前の重複を確認して、同じ名前の人がいる場合は入室拒否

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -1,8 +1,21 @@
 'use strict';
 
+global.memberList = []
+
 module.exports = function (socket) {
-    // 入室メッセージをクライアントに送信する
-    socket.on('enterRoom', function (data) {
-      socket.broadcast.emit('enterRoom', data)
-    });
+  // 入室メッセージをクライアントに送信する
+  socket.on('enterRoom', function (data) {
+    global.memberList.push(data);
+    console.log('enter memberList: ', global.memberList);
+    socket.broadcast.emit('enterRoom', data)
+  });
+
+  socket.on('checkNameDeplicate', function (userName) {
+    // 存在していたら、入室できないフラグ
+    if (global.memberList.indexOf(userName) != -1) {
+      socket.emit('blockEnterFlag', true);
+    } else {
+      socket.emit('blockEnterFlag', false);
+    }
+  });
 };
